633ad29a3a37117cdde4b3fa201d9416befc61b6
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-05 21:49:18 +0530

    Registering the model to admin .

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category,Product
 
 # Register your models here.
+
+# first register the Category
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+    
+    
+    
+# second register the Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','price',
+                    'available','created','updated']
+    list_filter = ['available','created','updated']
+    list_editable = ['price','available']
+    prepopulated_fields = {'slug' : ('name',)}
