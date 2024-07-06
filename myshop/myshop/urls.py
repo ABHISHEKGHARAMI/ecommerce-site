d7d39921fe11344b2663e95d4f6bf15ec8f59b25
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-06 12:45:17 +0530

    .mod

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls',namespace='shop')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
