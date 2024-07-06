6db7ed1db3cc6b8604cd966b2e87230b612a4e5e
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-06 11:56:55 +0530

    .mod

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -19,6 +20,9 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=self.slug)
     
 
 
@@ -46,4 +50,8 @@ class Product(models.Model):
         ]  
         
         def __str__(self):
-            return self.name      
+            return self.name     
+        
+        
+        def get_absolute_url(self):
+            return reverse('shop:product_detail',args=[self.id,self.slug]) 
