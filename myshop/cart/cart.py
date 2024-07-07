b76b988e519c7c430013ca699adbfa68fb332b11
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-07 11:36:50 +0530

    .mod

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -62,3 +62,7 @@ class Cart:
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
         yield item
+        
+        
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
