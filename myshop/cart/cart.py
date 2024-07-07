7d733104fe28cbfc31120864200bcccc266b785a
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-07 11:19:09 +0530

    Adding the cart for the user.

---
+++ myshop/cart/cart.py
@@ -0,0 +1,38 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+
+class Cart:
+    def __init__(self,request):
+        self.sessions = request.sessions
+        cart = self.sessions(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.sessions[settings.CART_SESSION_ID]={}
+        self.cart = cart
+        
+        
+    # add the items for the cart
+    def add(self,product,quantity=1,override_quantity=False):
+        """
+        Add a product to the cart or update its quantity.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity' : 0,
+                'price' : str(product.price)
+            }
+            
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        
+        self.save()
+        
+    
+    # save the cart for the user
+    def save(self):
+        # mark the session as "modified" to make sure it gets saved
+        self.sessions.modified = True
